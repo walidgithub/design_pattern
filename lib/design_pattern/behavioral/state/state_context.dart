febe23def783ef2ca207c9d24d7676e3a1ebafd5
walid barakat <leadermatrix2@yahoo.com>
2022-09-11 13:37:58 +0200

    11-9 state3

--- lib/design_pattern/behavioral/state/state_context.dart
+++ lib/design_pattern/behavioral/state/state_context.dart
@@ -10,7 +10,7 @@ class StateContext {
   Sink<IState> get _inState => _stateStream.sink;
   Stream<IState> get outState => _stateStream.stream;
 
-  IState _currentState;
+  IState? _currentState;
 
   StateContext() {
     _currentState = NoResultsState();
@@ -27,14 +27,14 @@ class StateContext {
   }
 
   void _addCurrentStateToStream() {
-    _inState.add(_currentState);
+    _inState.add(_currentState!);
   }
 
   Future<void> nextState() async {
-    await _currentState.nextState(this);
+    await _currentState?.nextState(this);
 
     if (_currentState is LoadingState) {
-      await _currentState.nextState(this);
+      await _currentState?.nextState(this);
     }
   }
 }
